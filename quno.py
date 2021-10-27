6e34dec77de93ba228b79fb554dab1ae6d870292
Ice <richard.jun.noh@gmail.com>
2021-10-27 12:54:25 -0500

    Add win condition and input check

--- quno.py
+++ quno.py
@@ -1,13 +1,13 @@
 import os
 import platform
-from player import Player
+import player
 
 # Main Game Logic
 
 class Game:
     """ Manages the current state of the game
 
-
+    This handles the main interface between the users and this program.
     """
 
     def __init__(self, numPlayers):
@@ -20,16 +20,22 @@ class Game:
 
     def initialize_players(self, numPlayers):
         for i in range(numPlayers):
-            self.players.append(Player(i))
+            self.players.append(player.Player(i))
 
     def play_turn(self):
         print("Player " + str(self.playerIndex + 1) + ", it's your turn.")
         currentPlayer = self.players[self.playerIndex]
-        currentPlayer.print_hand()
+        print(currentPlayer)
         cardSelectionIndex = int(input("Select a Card! (0 - " + str(len(currentPlayer.cards) - 1) + "): "))
         playedCard = currentPlayer.cards[cardSelectionIndex]
         playedCard.measure()
         playedCard.action()
+
+        # Check win condition
+        if len(currentPlayer.cards) == 0:
+            self.win()
+
+
         self.playerIndex = (self.playerIndex + 1) % len(self.players)
         
         # clear_console()
@@ -46,19 +52,31 @@ class Game:
         self.play_turn()
 
     def win(self):
-        pass
+        print("CONGRATULATIONS! YOU WON, PLAYER " + str(self.playerIndex + 1) + "!")
 
     def start_game(self):
         self.play_turn()
-        
-
 
 def clear_console():
     os_name = platform.system
-    if platform.system() == "Windows":
+    if os_name == "Windows":
         os.system("cls")   # Windows
     else:
         os.system("clear") # Linux/Mac
+
+def validInput(outString, lowerLimit, upperLimit):
+    isValid = False
+    givenInput = ""
+    while not isValid:
+        givenInput = input(outString)
+        if not givenInput.isnumeric():
+            outString = " Please try again with a number: "
+        elif not lowerLimit <= int(givenInput) < upperLimit:
+            outString = " Please enter a number between " + str(lowerLimit) + " to " + str(upperLimit-1) + ": "
+        else:
+            isValid = True
+    
+    return givenInput
 
 if __name__ == "__main__":
     clear_console()
@@ -76,7 +94,7 @@ if __name__ == "__main__":
     print("     Developed by Abdullah Assaf, Emily Padilla, and Richard Noh")
     print("     For TODO...")
     print("")
-    playerTotal = input("To start the game, please enter in the number of players for this game of QUNO: ")
+    playerTotal = validInput("To start the game, please enter in the number of players for this game of QUNO: ", 3, 9)
     print("Before the game starts, please decide amongst yourselves who will be player\n1, 2, and etc.")
     input("Once you have decided, please press Enter:")
 
